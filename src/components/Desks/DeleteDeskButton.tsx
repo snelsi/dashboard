f84a2835cab440503ac9f8a3e8680a12e94f3746
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-11 22:01:16 +0300

    Add 'delete desk' button

---
+++ src/components/Desks/DeleteDeskButton.tsx
@@ -0,0 +1,40 @@
+import * as React from "react";
+import styled from "styled-components";
+import { useHistory } from "react-router-dom";
+
+import { useDeleteDesk } from "utils/hooks";
+
+import { TrashButton } from "components";
+
+const DeleteButton = styled.div`
+  transition: all 0.2s ease-out;
+
+  opacity: 0;
+
+  &[data-hidden="false"] {
+    opacity: 1;
+  }
+`;
+
+interface DeleteDeskProps {
+  id: number;
+  hide?: boolean;
+}
+
+export const DeleteDeskButton: React.FC<DeleteDeskProps> = ({
+  id,
+  hide = false,
+}) => {
+  const history = useHistory();
+  const [deleteDesk, { loading }] = useDeleteDesk(id);
+
+  const deleteThisDesk = () => {
+    deleteDesk().then(() => history.push("/"));
+  };
+
+  return (
+    <DeleteButton data-hidden={hide}>
+      <TrashButton onClick={() => deleteThisDesk()} disabled={loading} />
+    </DeleteButton>
+  );
+};
