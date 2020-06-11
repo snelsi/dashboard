b83cf11c38900de989f0cc7abbc55a90962e7a2c
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-11 21:47:58 +0300

    Add useDesks hook

---
+++ src/utils/hooks/Desk/useDesks.ts
@@ -0,0 +1,7 @@
+import { useQuery } from "@apollo/client";
+import { GET_DESKS, GET_DESKS_VALUE } from "apollo";
+
+/**
+ * Returns list of desks without their content
+ */
+export const useDesks = () => useQuery<GET_DESKS_VALUE>(GET_DESKS);
