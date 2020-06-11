4a4f269368cfdb89d05d8b06897b78f0c40abce1
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-11 22:04:06 +0300

    Handle desks drag context

---
+++ src/components/Desks/DesksCatalog.tsx
@@ -0,0 +1,58 @@
+import * as React from "react";
+import styled from "styled-components";
+import { useMedia } from "react-use";
+import { Droppable } from "react-beautiful-dnd";
+
+import { CreateDesk } from "components/Desks";
+
+const Catalog = styled.div`
+  display: flex;
+  padding: 32px;
+  height: fit-content;
+  width: fit-content;
+
+  & > * {
+    height: 200px;
+    width: 300px;
+  }
+
+  @media (max-width: 600px) {
+    flex-direction: column;
+    width: 100%;
+
+    & > * {
+      width: 100%;
+    }
+  }
+`;
+
+interface DesksCatalogProps {
+  numberOfDesks: number;
+}
+
+export const DesksCatalog: React.FC<DesksCatalogProps> = ({
+  children,
+  numberOfDesks,
+}) => {
+  const isMobile = useMedia("(max-width: 600px)");
+
+  return (
+    <Droppable
+      droppableId="Desks-Catalog"
+      direction={isMobile ? "vertical" : "horizontal"}
+    >
+      {(provided) => (
+        <Catalog
+          {...provided.droppableProps}
+          ref={provided.innerRef}
+          className="desksCatalog"
+          data-testid="deskscatalog"
+        >
+          {children}
+          {provided.placeholder}
+          <CreateDesk index={numberOfDesks} />
+        </Catalog>
+      )}
+    </Droppable>
+  );
+};
