11d4b811cc22dcbe0341f289e412f22dae0d1675
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-11 22:11:12 +0300

    Add Task component

---
+++ src/components/Task/Task.tsx
@@ -0,0 +1,69 @@
+import * as React from "react";
+import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
+
+import type { ITask } from "interfaces";
+import { EditTaskModalForm, Status } from "components/Task";
+
+interface TaskProps {
+  task: ITask;
+  index: number;
+}
+
+const Wrapper = styled.div`
+  background-color: white;
+  border-radius: 0.25em;
+  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.25);
+
+  cursor: pointer;
+
+  color: #172b4d;
+  padding: 0.5em 1em;
+  position: relative;
+  overflow: hidden;
+
+  background-color: rgba(248, 248, 250, 1);
+  box-shadow: 0 1px 0 rgba(9, 30, 66, 0.2);
+`;
+
+const Tag = styled.div`
+  bottom: 0px;
+  font-weight: 700;
+  font-size: 24px;
+  opacity: 0.15;
+  position: absolute;
+  right: 16px;
+  user-select: none;
+  z-index: 0;
+`;
+
+export const Task: React.FC<TaskProps> = ({ task, index }) => {
+  const [mode, setMode] = React.useState(false);
+
+  return (
+    <>
+      <Draggable draggableId={String(task.id)} index={index}>
+        {(provided) => (
+          <Wrapper
+            className="task"
+            onClick={() => setMode(true)}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+            ref={provided.innerRef}
+          >
+            <Tag>{task.id}</Tag>
+            <Status data-status={task.status} />
+            <div>{task.description}</div>
+          </Wrapper>
+        )}
+      </Draggable>
+      <EditTaskModalForm
+        isOpen={mode}
+        closeModal={() => setMode(false)}
+        id={task.id}
+        initialDescription={task.description}
+        initialStatus={task.status}
+      />
+    </>
+  );
+};
