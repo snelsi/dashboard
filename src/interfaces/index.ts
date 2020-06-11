472ee5b99fb121e9a2e7ef66e623e367f3a20f3b
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-11 21:40:55 +0300

    Add Interfaces for Desk, Group and Task

---
+++ src/interfaces/index.ts
@@ -0,0 +1,24 @@
+export interface IDesk {
+  id: number;
+  name: string;
+  index: number;
+}
+
+export interface IGroup {
+  id: number;
+  name: string;
+  index: number;
+  desk_id: number;
+  tasks: ITask[];
+}
+
+export interface ITask {
+  id: number;
+  description: string;
+  index: number;
+  status: Status;
+  created_at: Date;
+  updated_at: Date;
+}
+
+export type Status = "Planned" | "In Progress" | "Done";
