3f3050d111cc64c9393c4c60a742af2a800384aa
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-11 22:05:20 +0300

    Setup Desks main page

--- src/pages/Desks.tsx
+++ src/pages/Desks.tsx
@@ -1,20 +1,38 @@
 import * as React from "react";
 
+import { useDesks } from "utils/hooks";
+import { compareIndex } from "utils/scripts";
+
 import { GitHubButton, Header, WorkSpace } from "components";
+import { DesksCatalog, Desk, DesksDragContext } from "components/Desks";
 
 export const Desks: React.FC = () => {
+  const { data, loading, error } = useDesks();
+
   React.useEffect(() => {
     document.title = "Desks";
   }, []);
 
   return (
-    <>
+    <DesksDragContext>
       <Header>
         <h1>Desks</h1>
+        {(error && <div>{error.message}</div>) ||
+          (loading && <div>Loading...</div>)}
       </Header>
 
-      <WorkSpace>Desks Catalog</WorkSpace>
+      <WorkSpace>
+        {data?.desks && (
+          <DesksCatalog numberOfDesks={data.desks.length}>
+            {Array.from(data.desks)
+              .sort(compareIndex)
+              .map((desk) => (
+                <Desk desk={desk} key={desk.id} />
+              ))}
+          </DesksCatalog>
+        )}
+      </WorkSpace>
       <GitHubButton />
-    </>
+    </DesksDragContext>
   );
 };
