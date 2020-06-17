60e43c5f59b77ac7559c8c7d3df5d9169ca8f5b0
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-06-17 19:39:53 +0300

    Add Observer and Subject interfaces

--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -28,3 +28,26 @@ export enum SystemRole {
   Moderator = "Moderator",
   Admin = "Admin",
 }
+
+/**
+ * Интферфейс издателя объявляет набор методов для управлениями подписчиками.
+ */
+export interface Subject {
+  // Присоединяет наблюдателя к издателю.
+  attach: (observer: Observer) => void;
+
+  // Отсоединяет наблюдателя от издателя.
+  detach: (observer: Observer) => void;
+
+  // Уведомляет всех наблюдателей о событии.
+  notify: () => void;
+}
+
+/**
+ * Интерфейс Наблюдателя объявляет метод уведомления, который издатели
+ * используют для оповещения своих подписчиков.
+ */
+export interface Observer {
+  // Получить обновление от субъекта.
+  update: (subject: Subject) => void;
+}
